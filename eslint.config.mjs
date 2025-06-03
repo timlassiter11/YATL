8bed2df5d1cbc7352da4921c764be91c5f6abda1
Timothy Lassiter <timlassiter11@gmail.com>
2025-06-03 11:02:16 -0400

    Added scripts for linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      "examples"
+    ]
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+);

a60a3e96cfaad2dffc280a26f32825d7ca8a25e0
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2026-02-10 11:15:53 -0500

    feat: added date filter component

--- src/filter-components/index.ts
+++ src/filter-components/index.ts
@@ -1,3 +1,4 @@
 export * from './yatl-base-filter';
+export * from './yatl-date-filter';
 export * from './yatl-search-filter';
 export * from './yatl-select-filter';
---
+++ src/filter-components/yatl-date-filter/yatl-date-filter.ts
@@ -0,0 +1,35 @@
+import { html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import theme from '../../theme';
+import styles from './yatl-date-filter.styles';
+import { YatlBaseFilter } from '../yatl-base-filter';
+import { live } from 'lit/directives/live.js';
+import { YatlDateInput } from '../../form-controls';
+
+@customElement('yatl-date-filter')
+export class YatlDateFilter extends YatlBaseFilter<Date> {
+  public static override styles = [theme, styles];
+
+  protected override render() {
+    return html`
+      <yatl-date-input
+        name=${this.field}
+        label=${this.label}
+        .value=${live(this.value)}
+        @change=${this.handleChange}>
+      </yatl-date-input>
+    `;
+  }
+
+  private handleChange(event: Event) {
+    event.stopPropagation();
+    const target = event.target as YatlDateInput;
+    this.value = target.value ? new Date(target.value) : undefined;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'yatl-date-filter': YatlDateFilter;
+  }
+}

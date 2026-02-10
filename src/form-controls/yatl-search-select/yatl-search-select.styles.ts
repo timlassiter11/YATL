37e6047895b042bd5204e9ec93f2433a088180ad
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2026-02-10 10:27:11 -0500

    fix(search-select): better height calculation

--- src/form-controls/yatl-search-select/yatl-search-select.styles.ts
+++ src/form-controls/yatl-search-select/yatl-search-select.styles.ts
@@ -2,8 +2,26 @@ import { css } from 'lit';
 
 export default css`
   :host {
-    --option-height: var(--yatl-select-option-height, 45px);
+    --option-height: var(--yatl-select-option-height, 35px);
     --options-gap: var(--yatl-select-options-gap, 0);
+    /* We need individual padding variables to calculate text-input height */
+    --input-padding-left: var(
+      --yatl-select-input-padding-left,
+      var(--yatl-spacing-m)
+    );
+    --input-padding-right: var(
+      --yatl-select-input-padding-right,
+      var(--yatl-spacing-m)
+    );
+    --input-padding-top: var(
+      --yatl-select-input-padding-top,
+      var(--yatl-spacing-m)
+    );
+    --input-padding-bottom: var(
+      --yatl-select-input-padding-bottom,
+      var(--yatl-spacing-m)
+    );
+
     --input-separator-width: var(--yatl-select-input-separator-width, 1px);
     --input-separator-color: var(
       --yatl-select-input-separator-color,
@@ -21,7 +39,10 @@ export default css`
   .text-input {
     display: flex;
     flex-direction: column;
-    height: calc(var(--option-height) * var(--size, 4));
+    height: calc(
+      var(--option-height) * var(--size, 4) + var(--input-padding-top) +
+        var(--input-padding-bottom)
+    );
     overflow: auto;
     justify-content: flex-start;
     padding: 0;
@@ -30,7 +51,10 @@ export default css`
   /* Need the specificity to override form control styles */
   .text-input > input[part='search'] {
     flex: 0 0 0%;
-    padding: var(--input-padding);
+    padding-left: var(--input-padding-left);
+    padding-right: var(--input-padding-right);
+    padding-top: var(--input-padding-top);
+    padding-bottom: var(--input-padding-bottom);
     border-bottom-width: var(--input-separator-width);
     border-bottom-style: solid;
     border-bottom-color: var(--input-separator-color);
@@ -45,9 +69,19 @@ export default css`
     padding: var(--yatl-spacing-m);
   }
 
+  /* Make all options the same size */
+  yatl-option,
+  ::slotted(yatl-option) {
+    flex-shrink: 0;
+    height: var(--option-height);
+  }
+
+  yatl-option::part(check) {
+    opacity: 0;
+  }
+
   yatl-option {
     --yatl-option-hover-bg: var(--yatl-color-danger);
-    height: var(--option-height);
   }
 
   yatl-option:hover [part='selected-trash-icon'] {

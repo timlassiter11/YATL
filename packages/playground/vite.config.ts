755475507f69580ac8037cf3d695e6dcab9aa42b
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2026-02-13 15:08:31 -0500

    chore: better debugging

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   base: '/YATL/',
   server: {
     port: 5174,
+    fs: {
+      allow: ['..'],
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@timlassiter11/yatl/theme.css',
+        replacement: path.resolve(__dirname, '../yatl/src/theme.css'),
+      },
+      {
+        find: '@timlassiter11/yatl',
+        // Route them directly to the raw TypeScript source
+        replacement: path.resolve(__dirname, '../yatl/src/index.ts'),
+      },
+      {
+        // Intercept imports to the UI package
+        find: '@timlassiter11/yatl-ui',
+        replacement: path.resolve(__dirname, '../yatl-ui/src/index.ts'),
+      },
+    ],
   },
 });

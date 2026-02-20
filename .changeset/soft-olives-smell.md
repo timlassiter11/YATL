4ce8dee75633a371b49b61006c427a36e54f9053
Timothy Lassiter <tim.lassiter@ruggedscience.com>
2026-02-20 22:44:54 +0000

    feat(option): added new cancellable toggle request event

---
+++ .changeset/soft-olives-smell.md
@@ -0,0 +1,5 @@
+---
+'@timlassiter11/yatl-ui': patch
+---
+
+Added new cancellable toggle request event for checkable options
--- packages/yatl-ui/src/events/index.ts
+++ packages/yatl-ui/src/events/index.ts
@@ -1,5 +1,6 @@
 export * from './dialog';
 export * from './dropdown';
+export * from './option';
 export * from './table-toolbar';
 export * from './table-view';
 export * from './tag';
